8e54e76faae3c7cc77c4bce78585476849de0ee1
emilru1 <emilru@chalmers.se>
2025-03-06 08:29:00 +0100

    Update Scania.java

--- ModelCar/Scania.java
+++ ModelCar/Scania.java
@@ -1,58 +1,41 @@
 package ModelCar;
 
 import java.awt.*;
-public class Scania extends Vehicle implements TruckBed{
-    private double tippingAngle;
-    private final double maxTippingAngel = 70.0;
-
-    //public final static double trimFactor = 1.50;
+public class Scania extends Vehicle {
+    private final BedFunc bedFunc;
 
     public Scania() {
+
         super(2, 125,"Scania", Color.white);
+        bedFunc = new BedFunc(this);
+        bedFunc.setBedVehicle(this);
+
     }
+
     public double speedFactor(){
         return enginePower * 0.01; // 1.5
     }
 
-    @Override
-    public double getTippingAngle(){
-        return tippingAngle;
+    public double getTippingAngle() {
+        return bedFunc.getTippingAngle();
     }
 
-    @Override
     public void increaseTipping(double angle) {
-        if (getCurrentSpeed() != 0){
-            throw new IllegalArgumentException("det går inte tippa när man kör");
-        }
-        if (angle < 0){
-            throw new IllegalArgumentException("Angel kan inte var minder än noll");
-        }
-
-        tippingAngle = Math.min(tippingAngle + angle, maxTippingAngel );
+        bedFunc.increaseTipping(angle);
     }
 
-    @Override
-    public void decreaseTipping(double angle){
-        if (getCurrentSpeed() != 0){
-            throw new IllegalArgumentException("det går inte tippa när man kör");
-        }
-        if (angle < 0){
-            throw new IllegalArgumentException("Angel kan inte var minder än noll");
-        }
-        tippingAngle = Math.max(tippingAngle - angle, 0 );
+    public void decreaseTipping(double angle) {
+        bedFunc.decreaseTipping(angle);
     }
 
     @Override
     public void move(){
-        if (tippingAngle > 0){
+        if (bedFunc.getTippingAngle() > 0){
             if (getCurrentSpeed()!= 0){
                 currentSpeed = 0;
                 System.out.println("Scania kan köras om flaket är uppe: ");
             }
-
         }
         super.move();
     }
-
-
 }
