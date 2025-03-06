9ada992b3936fa805daeb6e5d66d343d82e1c4f6
emilru1 <emilru@chalmers.se>
2025-03-06 08:29:17 +0100

    Update Iveco.java

--- ModelCar/Iveco.java
+++ ModelCar/Iveco.java
@@ -1,124 +1,66 @@
 package ModelCar;
 
-import ModelCar.PersonalCar;
-import ModelCar.TruckRamp;
-import ModelCar.Vehicle;
-
 import java.awt.*;
 import java.util.ArrayList;
 
-public class Iveco extends Vehicle implements TruckRamp {
-    private boolean rampUp;
-    private boolean loadMode;
-    private boolean fairDistance;
-    private int maxCapacity;
-    private ArrayList <PersonalCar> loadedCars;
+public class Iveco extends Vehicle  {
+    private final RampFunc rampFunc;
 
     public Iveco() {
         super(2, 130,"Iveco", Color.BLUE);
-        this.rampUp = true;
-        this.loadMode = false;
-        this.fairDistance = true;//  rimlig distans// ce med godsen;
-        this.maxCapacity = 4;
-        this.loadedCars = new ArrayList<>(maxCapacity);
+        rampFunc = new RampFunc(this);
+        rampFunc.setRampVehicle(this);
     }
 
     public double speedFactor() {
 
         return enginePower * 0.01;
     }
-    @Override
     public void startLoadMode(){
-        if (getCurrentSpeed() > 0){
-            throw new IllegalArgumentException("Lastbilen står ej stilla.");
-        }
-        if (this.loadMode) {
-            throw new IllegalArgumentException("Lastbilen är redan i LoadMode.");
-        }
-        this.loadMode = true;
-
+         rampFunc.startLoadMode();
     }
-    @Override
+
     public void exitLoadMode() {
-        if (!this.loadMode) {
-            throw new IllegalArgumentException("Lastbilen är inte i LoadMode.");
-        }
-        this.loadMode = false;
+         rampFunc.exitLoadMode();
     }
-    @Override
+
     public boolean getLoadMode(){
-        return this.loadMode;
+        return rampFunc.getLoadMode();
     }
-    @Override
+
     public void setRampDown(){
-        if (!this.loadMode){
-            throw new IllegalArgumentException("Lastbilen är ej i LoadMode.");
-        }
-        if (!this.rampUp) {
-            throw new IllegalArgumentException("Rampen är redan nere.");
-        }
-        this.rampUp = false;
+        rampFunc.setRampDown();
     }
-    @Override
+
     public void setRampUp(){
-        if (!this.loadMode){
-            throw new IllegalArgumentException("Lastbilen är ej i LoadMode.");
-        }
-        if (this.rampUp) {
-            throw new IllegalArgumentException("Rampen är redan uppe.");
-        }
-        this.rampUp = true;
+        rampFunc.setRampUp();
     }
 
-    @Override
     public boolean getIsRampUp() {
-        return this.rampUp;
+        return rampFunc.getIsRampUp();
     }
 
-    @Override
+
     public void loadCar(PersonalCar personalCar){
-        if (this.rampUp || !this.loadMode){
-            throw new IllegalArgumentException("Lastbilen skall stå still, ha rampen nere och vara i LoadMode.");
-        }
-        if (!this.fairDistance){
-            throw new IllegalArgumentException("Lastbilen är inte på ett rimligt avstånd till rampen.");
-        }
-        if (this.loadedCars.size() >= maxCapacity){
-            throw new IllegalArgumentException("Lastbilen kan inte lasta fler än " + maxCapacity + " bilar.");
-
-        }
-        this.loadedCars.add(personalCar);
+        rampFunc.loadCar(personalCar);
 
     }
 
-    @Override
     public void setFairDistance(boolean boolDistance) {
-        if (this.fairDistance == boolDistance){
-            throw new IllegalArgumentException("FairDistance är redan det värdet.");
-        }
-
-        this.fairDistance = boolDistance;
+        rampFunc.setFairDistance(boolDistance);
     }
 
-    @Override
+
     public boolean getFairDistance() {
-        return this.fairDistance;
+        return rampFunc.getFairDistance();
     }
 
-    @Override
     public void unloadCar(){
-        if (this.rampUp || !this.loadMode){
-            throw new IllegalArgumentException("Lastbilen ska stå stilla, ha rampen nere och vara i LoadMode.");
-        }
-        if (this.loadedCars.isEmpty()){
-            throw new IllegalArgumentException("Lastbilen har ingen bil lastad.");
-        }
-
-        this.loadedCars.remove(this.loadedCars.size() - 1);
+        rampFunc.unloadCar();
     }
-    @Override
+
     public ArrayList<PersonalCar> getLoadedCars() {
-        return new ArrayList<>(this.loadedCars);
+        return rampFunc.getLoadedCars();
 
     }
 }
